a0cc222901b621197c9a30e1194bfd46b3384f2e
Shubham555 <118034649+Shubham555@users.noreply.github.com>
2023-07-05 13:25:01 +0530

    Add files via upload

---
+++ build/precache-manifest.f075a53dff02e5d17fb450543c37ff62.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5f5991ff6cb3fd8fa29923407d4c7556",
+    "url": "/index.html"
+  },
+  {
+    "revision": "70c3ff1a8dd6beff5ca9",
+    "url": "/static/css/2.c46397a6.chunk.css"
+  },
+  {
+    "revision": "0e4c6907368d6907ea8b",
+    "url": "/static/css/main.8a79c027.chunk.css"
+  },
+  {
+    "revision": "70c3ff1a8dd6beff5ca9",
+    "url": "/static/js/2.4254f81d.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.4254f81d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0e4c6907368d6907ea8b",
+    "url": "/static/js/main.b0c74d0c.chunk.js"
+  },
+  {
+    "revision": "44d459044b9da8ae1201",
+    "url": "/static/js/runtime-main.6829a894.js"
+  }
+]);
